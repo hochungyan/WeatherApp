7fed7254b3144aa87ee1e52e831aab9b1df3bd6c
Justin Ho <hochungyan@live.hk>
2023-08-04 13:26:03 +0100

    some styling

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Search from './components/search/search';
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <Search />
     </div>
   );
