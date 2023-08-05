771f86eae35bcd9ffbd2528a403b9025d9ad685d
Justin Ho <hochungyan@live.hk>
2023-08-05 06:33:56 +0100

    React Search City bar created

--- react-weather-app/src/components/search/search.js
+++ react-weather-app/src/components/search/search.js
@@ -1,5 +1,19 @@
-const Search = () =>
+import {useState} from "react";
+import {AsyncPaginate} from "react-select-async-paginate";
+const Search = ({onSearchChange}) =>
 {
-    return ('Hello')
+    const [search, setSearch] =useState(null);
+    const handleOnChange =(searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+    }
+    return (
+        <AsyncPaginate
+        placeholder ="Search for city"
+        debounceTimeout={600}
+        value={search}
+        onChange={handleOnChange}
+        />
+    )
 }
 export default Search;
