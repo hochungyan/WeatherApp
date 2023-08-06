800b7911dec1696b4d8af611e94741950e39f366
Justin Ho <hochungyan@live.hk>
2023-08-06 07:55:24 +0100

    Fetching and mapping data from weather API

--- react-weather-app/src/api.js
+++ react-weather-app/src/api.js
@@ -8,4 +8,6 @@ export const GeoApiOptions = {
    
 };
 
-export const GEO_API_URL ='https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY= "c3ad59141801ed435db41c02d355ec9d";
